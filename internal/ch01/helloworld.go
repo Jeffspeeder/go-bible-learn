ca1044be5062e5120683117929f53578cd6d603c
Jeff <jeffspeeder.sg@gmail.com>
2025-03-17 23:49:59 +0800

    Chapter 01, helloworld

---
+++ internal/ch01/helloworld.go
@@ -0,0 +1,9 @@
+package ch01
+
+import (
+	"fmt"
+)
+
+func HelloWorld() {
+	fmt.Println("Hello World")
+}
---
+++ test/ch01-test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"go-bible-learn/internal/ch01"
+)
+
+func main() {
+	ch01.HelloWorld()
+}
